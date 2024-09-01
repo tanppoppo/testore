8c475e11ad005f19db84735b7c7320747077d9a4
gyahury <rudgns1243@naver.com>
2024-09-01 21:16:43 +0900

    Init: 초기 프로젝트 빌드 세팅
    - git action 빌드 테스트
    - entity 적용 빌드 테스트

--- src/main/java/com/tanppoppo/testore/testore/member/entity/MemberEntity.java
+++ src/main/java/com/tanppoppo/testore/testore/member/entity/MemberEntity.java
@@ -1,4 +1,31 @@
 package com.tanppoppo.testore.testore.member.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "member")
 public class MemberEntity {
+    @Id
+    @Column(name = "member_id", length = 20)
+    String memberId;
+
+    @Column(name = "member_password", nullable = false, length = 100)
+    String memberPassword;
+
+    @Column(name = "member_name", nullable = false, length = 20)
+    String memberName;
+
+    @Column(name = "phone", nullable = false, length = 20)
+    String phone;
 }
