984275e48df110e48ca2fb78fb7545d29e633e0f
MinCheolHa <mha26399@gmail.com>
2024-09-11 14:24:28 +0900

    Feat: WORD 관련 파일 추가
    - WordBookDTO.java 추가
    - WordBookEntity.java 추가
    - WordBookRespository.java 추가
    - WordController.java 수정
    - WordDTO.java 수정
    - WordEntity.java 수정
    - WordRepository.java 수정

---
+++ src/main/java/com/tanppoppo/testore/testore/word/dto/WordBookDTO.java
@@ -0,0 +1,29 @@
+package com.tanppoppo.testore.testore.word.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WordBookDTO {
+
+    private Integer wordbookID;
+    private String title;
+    private String content;
+    private String language;
+    private Integer languageLevel;
+    private Integer membershipLevel;
+    private Integer creatorId;
+    private Integer ownerId;
+    private String imagePath;
+    private Boolean publicOption;
+    private LocalDateTime studiedDate;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
