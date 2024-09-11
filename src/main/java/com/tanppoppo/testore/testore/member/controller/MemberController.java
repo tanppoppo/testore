8453a95a21216456c11d75fa68d080f680a8cf14
gyahury <rudgns1243@naver.com>
2024-09-11 20:05:27 +0900

    Design: 회원가입 폼 작성 및 헤더 구조 조정
    - 회원가입 기능 구현을 위한 폼 작성
    - 헤더 다른 메뉴인 경우 뒤로가기 추가
    - word-main.html 경로 변경
    - 컨트롤러 loginForm, joinForm 페이지 이동 추가

--- src/main/java/com/tanppoppo/testore/testore/member/controller/MemberController.java
+++ src/main/java/com/tanppoppo/testore/testore/member/controller/MemberController.java
@@ -1,4 +1,24 @@
 package com.tanppoppo.testore.testore.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("member")
 public class MemberController {
+
+    @GetMapping("loginForm")
+    public String loginForm() {
+        return "member/loginForm";
+    }
+
+    @GetMapping("joinForm")
+    public String joinForm() {
+        return "member/joinForm";
+    }
 }
