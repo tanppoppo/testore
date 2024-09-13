4772f42b637bdeced636cdd4e1a2c0f3599791e7
KIMGEON64 <kimgeon64@icloud.com>
2024-09-13 14:35:25 +0900

    Feat: ExamController.java 기능 추가
    - ExamController.java 기능 추가
    
    비고: ExamController.java 주석 참조

--- src/main/java/com/tanppoppo/testore/testore/exam/controller/ExamController.java
+++ src/main/java/com/tanppoppo/testore/testore/exam/controller/ExamController.java
@@ -1,8 +1,10 @@
 package com.tanppoppo.testore.testore.exam.controller;
 
+import com.tanppoppo.testore.testore.exam.service.ExamService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("exam")
 public class ExamController {
+
+    private final ExamService es;
+
+    /* 시험 기능 메인 페이지 이동 */
+    @GetMapping("")
+    public String examMain(){
+        return"exam/exam-main";
+    }
+
+    /* 시험지 생성 페이지 이동 */
+    @GetMapping("createForm")
+    public String createForm(){
+        return"exam/exam-create";
+    }
 
 }
