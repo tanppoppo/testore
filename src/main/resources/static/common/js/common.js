20ba06b084a945abdb764449e8213bd29b1a0d94
gyahury <rudgns1243@naver.com>
2024-09-17 19:58:52 +0900

    Feat: 토스트 숨기기 적용
    - 토스트 숨기기 적용
    - 공통 js 추가

---
+++ src/main/resources/static/common/js/common.js
@@ -0,0 +1,22 @@
+document.getElementById('hideToast').addEventListener('click', hideToast);
+const toastElement = document.querySelector('#toast');
+
+if (window.getComputedStyle(toastElement).display === 'flex') {
+    setTimeout(function () {
+        toastElement.style.display = 'none';
+    }, 3000)
+}
+
+function hideToast() {
+    const toastElement = document.querySelector('#toast');
+    toastElement.style.display = 'none';
+}
+
+function showToast() {
+    const toastElement = document.querySelector('#toast');
+    toastElement.style.display = 'flex';
+
+    setTimeout(function () {
+        toastElement.style.display = 'none';
+    }, 3000)
+}
