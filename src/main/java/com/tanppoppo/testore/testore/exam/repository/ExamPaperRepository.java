b23ef906559c6c52372c4cbd4fbff1b4b6cccac8
KIM GEON <kimgeon64@icloud.com>
2024-10-08 21:47:36 +0900

    Kimgeon64/exam (#25)
    * Refactor: ExamPaperRepository.java 코드 수정
    - ExamPaperRepository.java 코드 수정
    - ExamServiceImpl.java 코드 수정
    - BookmarkRepository.java 코드 추가
    - ItemLikeRepository.java 코드 추가
    - ReviewRepository.java 코드 추가
    
    비고 : 기능별 코드 분류사항 고려하여 수정
    
    * Feat: ExamController.java 공개여부 설정 기능 추가
    - ExamController.java 공개여부 설정 기능 추가
    - ExamServiceImpl.java 공개여부 설정 기능 추가
    - ExamServiceImpl.java 공개여부 설정 기능 추가
    
    * Feat: ExamController.java 리뷰 기능 추가
    - ExamController.java 리뷰 기능 추가
    - ExamService.java 리뷰 기능 추가
    - ExamServiceImpl.java 리뷰 기능 추가
    - ReviewRepository.java 기능 추가
    
    * Feat: ExamServiceImpl.java 기능추가
    
    - ExamServiceImpl.java 기능추가
    - ReviewDTO.java 기능추가
    - ReviewEntity.java 기능추가
    
    * Feat: ExamController.java 기능 추가
    
    - ExamPaperRepository.java 기능 추가
    - ExamService.java 기능 추가
    - ExamServiceImpl.java 기능 추가

--- src/main/java/com/tanppoppo/testore/testore/exam/repository/ExamPaperRepository.java
+++ src/main/java/com/tanppoppo/testore/testore/exam/repository/ExamPaperRepository.java
@@ -1,12 +1,14 @@
 package com.tanppoppo.testore.testore.exam.repository;
 
 import com.tanppoppo.testore.testore.exam.entity.ExamPaperEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -26,5 +28,20 @@ public interface ExamPaperRepository extends JpaRepository<ExamPaperEntity, Inte
     // 시험지 수
     @Query("SELECT COUNT(ep) FROM ExamPaperEntity ep WHERE ep.ownerId = :memberId")
     Integer countExamPapersByOwnerId(@Param("memberId") Integer memberId);
+
+    // 전체 시험지 랜덤 반환 -> publicOption 고려
+    @Query("SELECT ep FROM ExamPaperEntity ep WHERE ep.publicOption = true ORDER BY FUNCTION('RAND')")
+    List<ExamPaperEntity> findRandomExamPapers(Pageable pageable);
+
+    // 시험지 공유수 순위 정렬하여 Entity 반환 -> publicOption 고려
+    @Query("SELECT ep FROM ExamPaperEntity ep WHERE ep.publicOption = true GROUP BY ep ORDER BY COUNT(CASE WHEN ep.ownerId != ep.creatorId.memberId THEN 1 END) DESC")
+    List<ExamPaperEntity> findSortedExamPapersByShareCount(Pageable pageable);
+
+    // 주제 : 이번주 인기 시험지 추천
+    /* 조건 1. 현재날짜를 기준으로 월요일과 일요일 사이의 날짜만을 출력함.
+     * 조건 2. publicOption이 true인것만을 출력함.
+     * 조건 3. itemLikeEntity의 itemLikeId가 examPaperId와 같은 경우가 가장 많은 순서대로 정렬함. */
+    @Query("SELECT ep FROM ExamPaperEntity ep LEFT JOIN ItemLikeEntity il ON ep.examPaperId = il.itemId WHERE ep.publicOption = true AND il.createdDate BETWEEN :monday AND :sunday GROUP BY ep.examPaperId ORDER BY COUNT(il) DESC")
+    List<ExamPaperEntity> findPopularExamsThisWeek(@Param("monday") LocalDateTime monday, @Param("sunday") LocalDateTime sunday, Pageable pageable);
 
 }
