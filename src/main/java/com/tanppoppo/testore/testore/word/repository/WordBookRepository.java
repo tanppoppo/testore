9ae86c30b606f49dd29ab18b9646b00b658180df
MinCheolHa <mha26399@gmail.com>
2024-10-10 15:33:47 +0900

    Feat: 단어장 리뷰기능 및 검색화면의 추천, 인기, 많이 공유된 단어장 항목 기능 추가
    - 단어장 리뷰 목록 및 생성 및 저장, 수정, 삭제 기능 추가
    - 단어장 검색화면내 추천, 인기, 많이 공유된 단어장 기능 추가
    
    --------------------------------------
    
    DOCS: 단어장 메인 페이지 및 상세정보 수정
    
    - 기존 좋아요 및 북마크, 리뷰 기능의 경로 수정

--- src/main/java/com/tanppoppo/testore/testore/word/repository/WordBookRepository.java
+++ src/main/java/com/tanppoppo/testore/testore/word/repository/WordBookRepository.java
@@ -1,16 +1,22 @@
 package com.tanppoppo.testore.testore.word.repository;
 
 import com.tanppoppo.testore.testore.word.entity.WordBookEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface WordBookRepository extends JpaRepository<WordBookEntity, Integer> {
+
+    // 북마크 여부 고려 정렬 리스트 반환
+    @Query("SELECT ep FROM WordBookEntity ep LEFT JOIN BookmarkEntity bm ON ep.wordBookId = bm.itemId WHERE ep.ownerId = :ownerId ORDER BY CASE WHEN bm IS NOT NULL THEN 1 ELSE 0 END DESC, ep.wordBookId DESC")
+    List<WordBookEntity> findByOwnerIdWithBookmarks(@Param("ownerId") Integer ownerId, Sort sort);
 
     List<WordBookEntity> findByOwnerId(Integer ownerId);
 
@@ -37,7 +43,19 @@ public interface WordBookRepository extends JpaRepository<WordBookEntity, Intege
     Integer getShareCount(@Param("memberId") Integer memberId);
 
     // likeState : 좋아요 여부
-//    @Query("SELECT COUNT(le) > 0 FROM ItemLikeEntity le WHERE le.memberId.memberId = :memberId AND le.itemId = :examPaperId")
-//    Boolean getLikeState(@Param("memberId") Integer memberId, @Param("wordBookId") Integer wordBookId);
+    @Query("SELECT COUNT(le) > 0 FROM ItemLikeEntity le WHERE le.memberId.memberId = :memberId AND le.itemId = :examPaperId")
+    Boolean getLikeState(@Param("memberId") Integer memberId, @Param("wordBookId") Integer wordBookId);
+
+    // 전체 시험지 랜덤 반환 -> publicOption 고려
+    @Query("SELECT ep FROM WordBookEntity ep WHERE ep.publicOption = true ORDER BY FUNCTION('RAND')")
+    List<WordBookEntity> findRandomWordBooks(Pageable pageable);
+
+    // 시험지 공유수 순위 정렬하여 Entity 반환 -> publicOption 고려
+    @Query("SELECT ep FROM WordBookEntity ep WHERE ep.publicOption = true GROUP BY ep ORDER BY COUNT(CASE WHEN ep.ownerId != ep.creatorId.memberId THEN 1 END) DESC")
+    List<WordBookEntity> findSortedWordBooksByShareCount(Pageable pageable);
+
+    // 이번주 인기 시험지 추천 -> publicOption 고려
+    @Query("SELECT ep FROM WordBookEntity ep LEFT JOIN ItemLikeEntity il ON ep.wordBookId = il.itemId WHERE ep.publicOption = true AND il.createdDate BETWEEN :monday AND :sunday GROUP BY ep.wordBookId ORDER BY COUNT(il) DESC")
+    List<WordBookEntity> findPopularWordsThisWeek(@Param("monday") LocalDateTime monday, @Param("sunday") LocalDateTime sunday, Pageable pageable);
 
 }
