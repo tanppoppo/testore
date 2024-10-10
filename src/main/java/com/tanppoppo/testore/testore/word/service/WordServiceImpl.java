e6e6eae2917d3a38b7c0a3a5424da7190b3a9720
MinCheolHa <mha26399@gmail.com>
2024-10-10 15:41:50 +0900

    Min cheol ha/create word (#30)
    * Feat: 단어 추가 기능
    
    - 단어 추가 기능
    - dto, entity wordNum 추가
    
    * Feat: 단어장 북마크, 좋아요, 단어장 잠금 기능 추가 및 단어장 정보 조회기능 추가
    
    - 단어장 북마크 추가 및 삭제 기능 추가
    - 단어장 좋아요 추가 및 삭제 기능 추가
    - 단어장 공개여부 기능 추가
    - 업데이트할 단어장 정보 조회 기능 추가
    
    * Feat: 단어장 리뷰기능 및 검색화면의 추천, 인기, 많이 공유된 단어장 항목 기능 추가
    
    - 단어장 리뷰 목록 및 생성 및 저장, 수정, 삭제 기능 추가
    - 단어장 검색화면내 추천, 인기, 많이 공유된 단어장 기능 추가
    
    --------------------------------------
    
    DOCS: 단어장 메인 페이지 및 상세정보 수정
    
    - 기존 좋아요 및 북마크, 리뷰 기능의 경로 수정

--- src/main/java/com/tanppoppo/testore/testore/word/service/WordServiceImpl.java
+++ src/main/java/com/tanppoppo/testore/testore/word/service/WordServiceImpl.java
@@ -1,7 +1,13 @@
 package com.tanppoppo.testore.testore.word.service;
 
+import com.tanppoppo.testore.testore.common.util.ItemTypeEnum;
+import com.tanppoppo.testore.testore.member.dto.ReviewDTO;
 import com.tanppoppo.testore.testore.member.entity.MemberEntity;
+import com.tanppoppo.testore.testore.member.entity.ReviewEntity;
+import com.tanppoppo.testore.testore.member.repository.BookmarkRepository;
+import com.tanppoppo.testore.testore.member.repository.ItemLikeRepository;
 import com.tanppoppo.testore.testore.member.repository.MemberRepository;
+import com.tanppoppo.testore.testore.member.repository.ReviewRepository;
 import com.tanppoppo.testore.testore.security.AuthenticatedUser;
 import com.tanppoppo.testore.testore.word.dto.WordBookDTO;
 import com.tanppoppo.testore.testore.word.entity.WordBookEntity;
@@ -10,10 +16,14 @@ import com.tanppoppo.testore.testore.word.repository.WordBookRepository;
 import com.tanppoppo.testore.testore.word.repository.WordRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
@@ -23,6 +33,9 @@ public class WordServiceImpl implements WordService {
     private final WordBookRepository wbr;
     private final WordRepository wr;
     private final MemberRepository mr;
+    private final ReviewRepository rr;
+    private final ItemLikeRepository ilr;
+    private final BookmarkRepository br;
 
 
     /**
@@ -102,7 +115,7 @@ public class WordServiceImpl implements WordService {
     }
 
     /**
-     * 단어장 메인 리스트 조회
+     * 단어장 메인 페이지 이동
      * @author MinCheolHa
      * @param user 인증된 회원 정보를 가져옵니다.
      * @return List형식의 items를 반환합니다.
@@ -110,8 +123,12 @@ public class WordServiceImpl implements WordService {
     @Override
     public List<WordBookDTO> getListItems(AuthenticatedUser user) {
 
+        MemberEntity memberEntity = mr.findById(user.getId())
+                .orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
         Sort sort = Sort.by(Sort.Direction.DESC, "wordBookId");
-        List<WordBookEntity> wordBookEntityList = wbr.findByOwnerId(user.getId(), sort);
+        List<WordBookEntity> wordBookEntityList = wbr.findByOwnerIdWithBookmarks(memberEntity.getMemberId(), sort);
+
         List<WordBookDTO> items = new ArrayList<>();
 
         for (WordBookEntity entity : wordBookEntityList) {
@@ -121,8 +138,9 @@ public class WordServiceImpl implements WordService {
                     .content(entity.getContent())
                     .imagePath(entity.getImagePath())
                     .wordItemCount(wbr.getWordItemCount(entity.getWordBookId()))
-                    .likeCount(wbr.getLikeCount(entity.getWordBookId()))
+                    .likeCount(ilr.getLikeCount(entity.getWordBookId()))
                     .shareCount(wbr.getShareCount(entity.getCreatorId().getMemberId()))
+                    .isBookmarked(br.getBookmarkState(user.getId(), entity.getWordBookId(), ItemTypeEnum.WORD))
                     .build();
             items.add(wordBookDTO);
         }
@@ -133,19 +151,24 @@ public class WordServiceImpl implements WordService {
     /**
      * 단어장 상세 정보 조회
      * @author MinCheolHa
-     * @param wordbookId wordBookId 시험지 키값을 가져옵니다.
+     * @param wordBookId wordBookId 시험지 키값을 가져옵니다.
      * @return Map 객체 detail 을 반환합니다.
      */
     @Override
-    public Map<String, Object> selectWordBookDetail(int wordbookId, AuthenticatedUser user) {
+    public Map<String, Object> selectWordBookDetail(int wordBookId, AuthenticatedUser user) {
 
-        WordBookEntity wordBookEntity = wbr.findById(wordbookId)
+        WordBookEntity wordBookEntity = wbr.findById(wordBookId)
                 .orElseThrow(()-> new EntityNotFoundException("단어장 정보를 찾을 수 없습니다."));
 
         MemberEntity memberEntity = mr.findById(wordBookEntity.getCreatorId().getMemberId())
                 .orElseThrow(() -> new EntityNotFoundException("회원 정보를 찾을수 없습니다."));
 
+        if (!user.getId().equals(wordBookEntity.getOwnerId()) && !wordBookEntity.getPublicOption()){
+            throw new AccessDeniedException("공개된 단어장이 아닙니다.");
+        }
+
         WordBookDTO wordBookDTO = WordBookDTO.builder()
+                .wordBookId(wordBookEntity.getWordBookId())
                 .creatorId(wordBookEntity.getCreatorId().getMemberId())
                 .imagePath(wordBookEntity.getImagePath())
                 .title(wordBookEntity.getTitle())
@@ -154,15 +177,16 @@ public class WordServiceImpl implements WordService {
                 .ownerId(wordBookEntity.getOwnerId())
                 .publicOption(wordBookEntity.getPublicOption())
                 .wordItemCount(wbr.getWordItemCount(wordBookEntity.getWordBookId()))
-                .likeCount(wbr.getLikeCount(wordBookEntity.getWordBookId()))
+                .likeCount(ilr.getLikeCount(wordBookEntity.getWordBookId()))
                 .shareCount(wbr.getShareCount(wordBookEntity.getCreatorId().getMemberId()))
                 .build();
 
         Map<String, Object> detail = new HashMap<>();
         detail.put("wordBookDTO", wordBookDTO);
         detail.put("nickName", wordBookEntity.getCreatorId().getNickname());
-        detail.put("reviewCount", wbr.getReviewCount(wordbookId));
-//        detail.put("likeState", wbr.getLikeState(memberEntity.getMemberId(), wordbookId));
+        detail.put("reviewCount", rr.getReviewCount(wordBookId));
+        detail.put("likeState", ilr.getLikeState(memberEntity.getMemberId(), wordBookId, ItemTypeEnum.WORD));
+        detail.put("bookmarkState", br.getBookmarkState(memberEntity.getMemberId(), wordBookId, ItemTypeEnum.WORD));
         return detail;
 
     }
@@ -215,7 +239,252 @@ public class WordServiceImpl implements WordService {
 
         return wordBookDTO;
     }
+    /**
+     * 리뷰 목록 반환
+     * @author MinCheolHa
+     * @param user user 객체를 가져옵니다.
+     * @param wordBookId 단어장 키값을 가져옵니다.
+     * @return Map 객체 result를 반환합니다.
+     */
+    @Override
+    public Map<String,Object> getListReviews(AuthenticatedUser user, int wordBookId) {
 
+        wbr.findById(wordBookId).orElseThrow(()-> new EntityNotFoundException("단어장 정보를 찾을 수 없습니다."));
+
+        MemberEntity memberEntity = mr.findById(user.getId())
+                .orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "reviewId");
+        List<ReviewEntity> reviewEntityList = rr.findByItemIdAndItemType(wordBookId, ItemTypeEnum.WORD, sort);
+
+        List<ReviewDTO> reviewDTOList = new ArrayList<>();
+
+        for (ReviewEntity entity : reviewEntityList) {
+            ReviewDTO reviewDTO = ReviewDTO.builder()
+                    .rating(entity.getRating())
+                    .content(entity.getContent())
+                    .createdDate(entity.getCreatedDate())
+                    .updateDate(entity.getUpdateDate())
+                    .nickname(memberEntity.getNickname())
+                    .build();
+            reviewDTOList.add(reviewDTO);
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("reviewDTOList", reviewDTOList);
+
+        return result;
+
+    }
+
+    /**
+     * 시험지 리뷰 생성 정보 저장
+     * @author MinCheolHa
+     * @param user user 객체를 가져옵니다.
+     * @param wordBookId 단어장 키값을 가져옵니다.
+     */
+    @Override
+    public void createReview(AuthenticatedUser user, int wordBookId, ReviewDTO reviewDTO) {
+
+        wbr.findById(wordBookId).orElseThrow(()-> new EntityNotFoundException("시험지 정보가 존재하지 않습니다."));
+
+        MemberEntity memberEntity = mr.findById(user.getId())
+                .orElseThrow(()-> new EntityNotFoundException("회원 정보가 존재하지 않습니다."));
+
+        ReviewEntity reviewEntity = ReviewEntity.builder()
+                .rating(reviewDTO.getRating())
+                .content(reviewDTO.getContent())
+                .itemId(wordBookId)
+                .memberId(memberEntity)
+                .itemType(ItemTypeEnum.WORD)
+                .build();
+        rr.save(reviewEntity);
+
+    }
+
+    /**
+     * 리뷰 수정 페이지 이동
+     * @author MinCheolHa
+     * @param user user 객체를 가져옵니다.
+     * @param wordBookId 단어장 키값을 가져옵니다.
+     * @param reviewId 리뷰 키값을 가져옵니다.
+     * @return reviewDTO를 반환합니다.
+     */
+    @Override
+    public ReviewDTO selectUpdatedReviewInfo(AuthenticatedUser user, int wordBookId, int reviewId) {
+
+        wbr.findById(wordBookId).orElseThrow(()-> new EntityNotFoundException("단어장 정보를 찾을 수 없습니다."));
+
+        MemberEntity memberEntity = mr.findById(user.getId())
+                .orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        ReviewEntity reviewEntity = rr.findById(reviewId)
+                .orElseThrow(()-> new EntityNotFoundException("리뷰 정보를 찾을 수 없습니다."));
+
+        if (!reviewEntity.getMemberId().getMemberId().equals(memberEntity.getMemberId())){
+            throw new AccessDeniedException("인증회원과 리뷰정보가 일치하지 않습니다.");
+        }
+
+        ReviewDTO reviewDTO = ReviewDTO.builder()
+                .rating(reviewEntity.getRating())
+                .content(reviewEntity.getContent())
+                .build();
+
+        return reviewDTO;
+
+    }
+
+    /**
+     * 리뷰 수정
+     * @author MinCheolHa
+     * @param user user 객체를 가져옵니다.
+     * @param reviewId 리뷰 키값을 가져옵니다.
+     */
+    @Override
+    public void updateReview(AuthenticatedUser user, int reviewId, ReviewDTO reviewDTO) {
+
+        MemberEntity memberEntity = mr.findById(user.getId())
+                .orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        ReviewEntity reviewEntity = rr.findById(reviewId)
+                .orElseThrow(()-> new EntityNotFoundException("리뷰 정보를 찾을 수 없습니다."));
+
+        if (!reviewEntity.getMemberId().getMemberId().equals(memberEntity.getMemberId())){
+            throw new AccessDeniedException("인증회원과 리뷰정보가 일치하지 않습니다.");
+        }
+
+        reviewEntity.setRating(reviewDTO.getRating());
+        reviewEntity.setContent(reviewDTO.getContent());
+
+    }
+
+    /**
+     * 리뷰 삭제
+     * @author MinCheolHa
+     * @param user user 객체를 가져옵니다.
+     * @param reviewId 리뷰 키값을 가져옵니다.
+     */
+    @Override
+    public void deleteReview(AuthenticatedUser user, int reviewId) {
+
+        MemberEntity memberEntity = mr.findById(user.getId())
+                .orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        ReviewEntity reviewEntity = rr.findById(reviewId)
+                .orElseThrow(()-> new EntityNotFoundException("리뷰 정보를 찾을 수 없습니다."));
+
+        if (!reviewEntity.getMemberId().getMemberId().equals(memberEntity.getMemberId())){
+            throw new AccessDeniedException("인증회원과 리뷰정보가 일치하지 않습니다.");
+        }
+
+        rr.delete(reviewEntity);
+
+    }
+
+
+    /**
+     * 추천 단어장 반환
+     * @author MinCheolHa
+     * @param user user 객체를 가져 옵니다.
+     * @return WordBookDTO 형식의 recommendWord 를 반환 합니다.
+     */
+    @Override
+    public List<WordBookDTO> recommendedWordBook(AuthenticatedUser user) {
+        mr.findById(user.getId()).orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        Pageable pageable = PageRequest.of(0, 3);
+        List<WordBookEntity> wordBookEntity = wbr.findRandomWordBooks(pageable);
+
+        List<WordBookDTO> recommendedWordBook = new ArrayList<>();
+
+        for (WordBookEntity entity : wordBookEntity) {
+            WordBookDTO wordBookDTO = WordBookDTO.builder()
+                    .wordBookId(entity.getWordBookId())
+                    .title(entity.getTitle())
+                    .content(entity.getContent())
+                    .imagePath(entity.getImagePath())
+                    .wordItemCount(wbr.getWordItemCount(entity.getWordBookId()))
+                    .likeCount(ilr.getLikeCount(entity.getWordBookId()))
+                    .shareCount(wbr.getShareCount(entity.getCreatorId().getMemberId()))
+                    .build();
+            recommendedWordBook.add(wordBookDTO);
+
+        }
+
+        return recommendedWordBook;
+    }
+
+    /**
+     * 이번주 인기 단어장 반환
+     * @author MinCheolHa
+     * @param user user 객체를 가져 옵니다.
+     * @return WordBookDTO 형식의 popularityWord 를 반환 합니다.
+     */
+    @Override
+    public List<WordBookDTO> likedWordBook(AuthenticatedUser user) {
+
+        mr.findById(user.getId()).orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime monday = now.with(DayOfWeek.MONDAY).toLocalDate().atStartOfDay();
+        LocalDateTime sunday = now.with(DayOfWeek.SUNDAY).toLocalDate().atTime(23, 59, 59);
+
+        Pageable pageable = PageRequest.of(0, 3);
+        List<WordBookEntity> wordBookEntity = wbr.findPopularWordsThisWeek(monday, sunday, pageable);
+
+        List<WordBookDTO> likedWordBook = new ArrayList<>();
+
+        for (WordBookEntity entity : wordBookEntity) {
+            WordBookDTO wordBookDTO = WordBookDTO.builder()
+                    .wordBookId(entity.getWordBookId())
+                    .title(entity.getTitle())
+                    .content(entity.getContent())
+                    .imagePath(entity.getImagePath())
+                    .wordItemCount(wbr.getWordItemCount(entity.getWordBookId()))
+                    .likeCount(ilr.getLikeCount(entity.getWordBookId()))
+                    .shareCount(wbr.getShareCount(entity.getCreatorId().getMemberId()))
+                    .build();
+            likedWordBook.add(wordBookDTO);
+
+        }
+
+        return likedWordBook;
+
+    }
+
+    /**
+     * 많이 공유된 단어장 반환
+     * @author MinCheolHa
+     * @param user user 객체를 가져 옵니다.
+     * @return WordBookDTO 형식의 muchSharedWord 를 반환 합니다.
+     */
+    @Override
+    public List<WordBookDTO> muchSharedWordBook(AuthenticatedUser user) {
+
+        mr.findById(user.getId()).orElseThrow(()-> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
+
+        Pageable pageable = PageRequest.of(0, 3);
+        List<WordBookEntity> wordBookEntity = wbr.findSortedWordBooksByShareCount(pageable);
+
+        List<WordBookDTO> muchSharedWordBook = new ArrayList<>();
+
+        for (WordBookEntity entity : wordBookEntity) {
+            WordBookDTO wordBookDTO = WordBookDTO.builder()
+                    .wordBookId(entity.getWordBookId())
+                    .title(entity.getTitle())
+                    .content(entity.getContent())
+                    .imagePath(entity.getImagePath())
+                    .wordItemCount(wbr.getWordItemCount(entity.getWordBookId()))
+                    .likeCount(ilr.getLikeCount(entity.getWordBookId()))
+                    .shareCount(wbr.getShareCount(entity.getCreatorId().getMemberId()))
+                    .build();
+            muchSharedWordBook.add(wordBookDTO);
+
+        }
+
+        return muchSharedWordBook;
+
+    }
 
 
 }
