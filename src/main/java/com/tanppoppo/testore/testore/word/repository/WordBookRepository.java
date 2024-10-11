f62c10ad933ec77b7eb2a974cda2117afea66382
KIM GEON <kimgeon64@icloud.com>
2024-10-11 16:19:53 +0900

    Kimgeon64/exam (#32)
    * Refactor: ExamPaperRepository.java 코드 수정
    - ExamPaperRepository.java 코드 수정
    - ExamServiceImpl.java 코드 수정
    - BookmarkRepository.java 코드 추가
    - ItemLikeRepository.java 코드 추가
    - ReviewRepository.java 코드 추가
    
    비고 : 기능별 코드 분류사항 고려하여 수정
    
    * Feat: ExamController.java 공개여부 설정 기능 추가
    - ExamController.java 공개여부 설정 기능 추가
    - ExamServiceImpl.java 공개여부 설정 기능 추가
    - ExamServiceImpl.java 공개여부 설정 기능 추가
    
    * Feat: ExamController.java 리뷰 기능 추가
    - ExamController.java 리뷰 기능 추가
    - ExamService.java 리뷰 기능 추가
    - ExamServiceImpl.java 리뷰 기능 추가
    - ReviewRepository.java 기능 추가
    
    * Feat: ExamServiceImpl.java 기능추가
    
    - ExamServiceImpl.java 기능추가
    - ReviewDTO.java 기능추가
    - ReviewEntity.java 기능추가
    
    * Feat: ExamController.java 기능 추가
    
    - ExamPaperRepository.java 기능 추가
    - ExamService.java 기능 추가
    - ExamServiceImpl.java 기능 추가
    
    * Refactor: 메인 리스트 반환시 북마크 우선 반환하도록 수정
    
    - ExamPaperRepository.java 기능 수정
    - ExamServiceImpl.java 기능 수정
    - ExamController.java 주석 제거
    - ExamPaperDTO.java 변수 추가
    
    * Refactor: 공통 기능 재사용성 확대
    
    - BookmarkRepository.java 기능 수정
    - ItemLikeRepository.java 기능 수정
    - ReviewRepository.java 기능 수정
    - ExamServiceImpl.java 기능 수정
    
    * Feat: 시험지 찾기 기능 추가
    
    - ExamController.java 시험지 찾기 기능추가
    - ExamPaperRepository.java 시험지 찾기 기능추가, 시험지 메인 코드수정
    - ExamService.java 시험지 찾기 기능추가
    - ExamServiceImpl.java 시험지 찾기 기능추가, 시험지 메인 코드수정
    
    * Feat: 단어장 찾기 기능 추가
    
    - WordBookRepository.java 기능추가, 코드 수정
    - WordController.java 단어장 찾기 기능추가
    - WordService.java 단어장 찾기 기능추가
    - WordServiceImpl.java 단어장 찾기 기능추가, 코드 수정

--- src/main/java/com/tanppoppo/testore/testore/word/repository/WordBookRepository.java
+++ src/main/java/com/tanppoppo/testore/testore/word/repository/WordBookRepository.java
@@ -1,11 +1,11 @@
 package com.tanppoppo.testore.testore.word.repository;
 
+import com.tanppoppo.testore.testore.common.util.ItemTypeEnum;
 import com.tanppoppo.testore.testore.word.entity.WordBookEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -14,48 +14,40 @@ import java.util.List;
 @Repository
 public interface WordBookRepository extends JpaRepository<WordBookEntity, Integer> {
 
-    // 북마크 여부 고려 정렬 리스트 반환
-    @Query("SELECT ep FROM WordBookEntity ep LEFT JOIN BookmarkEntity bm ON ep.wordBookId = bm.itemId WHERE ep.ownerId = :ownerId ORDER BY CASE WHEN bm IS NOT NULL THEN 1 ELSE 0 END DESC, ep.wordBookId DESC")
-    List<WordBookEntity> findByOwnerIdWithBookmarks(@Param("ownerId") Integer ownerId, Sort sort);
+    // 북마크 상위 정렬 단어장 전체 반환 [ 검수 완료 ]
+    @Query("SELECT wb FROM WordBookEntity wb LEFT JOIN BookmarkEntity bm ON wb.wordBookId = bm.itemId AND bm.itemType = :itemType WHERE wb.ownerId = :ownerId ORDER BY CASE WHEN bm IS NOT NULL THEN 1 ELSE 0 END DESC, wb.wordBookId DESC")
+    List<WordBookEntity> findByOwnerIdWithBookmarks(Integer ownerId, ItemTypeEnum itemType);
 
-    List<WordBookEntity> findByOwnerId(Integer ownerId);
 
-    @Query("SELECT COUNT(wb) FROM WordBookEntity wb WHERE wb.ownerId = :ownerId")
-    Integer countByOwnerId(@Param("ownerId") Integer ownerId);
+    // 단어 수 [ 검수 완료 ]
+    @Query("SELECT COUNT(w) FROM WordEntity w WHERE w.wordBookId.wordBookId = :wordBookId")
+    Integer getWordItemCount(Integer wordBookId);
 
-    // memberId를 기준으로 wordBook을 정렬하여 List를 반환함
-    List<WordBookEntity> findByOwnerId(Integer id, Sort sort);
+    // 공유 수 [ 검수 완료 ]
+    @Query("SELECT SUM(CASE WHEN wb.ownerId != wb.creatorId.memberId THEN 1 ELSE 0 END) FROM WordBookEntity wb WHERE wb.creatorId.memberId = :memberId")
+    Integer getShareCount(Integer memberId);
 
-    // count : 단어수
-    @Query("SELECT COUNT(ei) FROM WordEntity ei WHERE ei.wordBookId.wordBookId = :wordBookId")
-    Integer getWordItemCount(@Param("wordBookId") Integer wordBookId);
+    // 단어장 수 [ 검수 완료 ]
+    @Query("SELECT COUNT(wb) FROM WordBookEntity wb WHERE wb.ownerId = :memberId")
+    Integer countWordBooksByOwnerId(Integer memberId);
 
-    // like  : 좋아요수
-    @Query("SELECT COUNT(l) FROM ItemLikeEntity l WHERE l.itemId = :wordBookId")
-    Integer getLikeCount(@Param("wordBookId") Integer wordBookId);
-
-    // reviews : 리뷰수
-    @Query("SELECT COUNT(r) FROM ReviewEntity r WHERE r.itemId = :wordBookId")
-    Integer getReviewCount(@Param("wordBookId") Integer wordBookId);
-
-    // share : 공유수
-    @Query("SELECT SUM(CASE WHEN ep.ownerId != ep.creatorId.memberId THEN 1 ElSE 0 END) FROM WordBookEntity ep WHERE ep.creatorId.memberId = :memberId")
-    Integer getShareCount(@Param("memberId") Integer memberId);
-
-    // likeState : 좋아요 여부
-//    @Query("SELECT COUNT(le) > 0 FROM ItemLikeEntity le WHERE le.memberId.memberId = :memberId AND le.itemId = :examPaperId")
-//    Boolean getLikeState(@Param("memberId") Integer memberId, @Param("wordBookId") Integer wordBookId);
-
-    // 전체 시험지 랜덤 반환 -> publicOption 고려
-    @Query("SELECT ep FROM WordBookEntity ep WHERE ep.publicOption = true ORDER BY FUNCTION('RAND')")
+    // 추천 단어장 [ 단어장 찾기 ]
+    @Query("SELECT wb FROM WordBookEntity wb WHERE wb.publicOption = true ORDER BY FUNCTION('RAND')")
     List<WordBookEntity> findRandomWordBooks(Pageable pageable);
 
-    // 시험지 공유수 순위 정렬하여 Entity 반환 -> publicOption 고려
-    @Query("SELECT ep FROM WordBookEntity ep WHERE ep.publicOption = true GROUP BY ep ORDER BY COUNT(CASE WHEN ep.ownerId != ep.creatorId.memberId THEN 1 END) DESC")
+    // 많이 공유된 단어장 [ 단어장 찾기 ]
+    @Query("SELECT wb FROM WordBookEntity wb WHERE wb.publicOption = true GROUP BY wb ORDER BY COUNT(CASE WHEN wb.ownerId != wb.creatorId.memberId THEN 1 END) DESC")
     List<WordBookEntity> findSortedWordBooksByShareCount(Pageable pageable);
 
-    // 이번주 인기 시험지 추천 -> publicOption 고려
-    @Query("SELECT ep FROM WordBookEntity ep LEFT JOIN ItemLikeEntity il ON ep.wordBookId = il.itemId WHERE ep.publicOption = true AND il.createdDate BETWEEN :monday AND :sunday GROUP BY ep.wordBookId ORDER BY COUNT(il) DESC")
-    List<WordBookEntity> findPopularWordsThisWeek(@Param("monday") LocalDateTime monday, @Param("sunday") LocalDateTime sunday, Pageable pageable);
+    // 이번주 인기 시험지 [ 단어장 찾기 ]
+    @Query("SELECT wb FROM WordBookEntity wb LEFT JOIN ItemLikeEntity il ON wb.wordBookId = il.itemId WHERE wb.publicOption = true AND il.createdDate BETWEEN :monday AND :sunday GROUP BY wb.wordBookId ORDER BY COUNT(il) DESC")
+    List<WordBookEntity> findPopularWordBooksThisWeek(LocalDateTime monday, LocalDateTime sunday, Pageable pageable);
+
+    // 공개된 시험지 전체 반환 [ 단어장 찾기 ]
+    List<WordBookEntity> findByPublicOption(boolean publicOption, Sort sort);
+
+    // 키워드와 일치한 공개된 시험지 전체 반환 [ 단어장 찾기 ]
+    @Query("SELECT wb FROM WordBookEntity wb WHERE wb.publicOption = :publicOption AND wb.title LIKE %:title%")
+    List<WordBookEntity> findByPublicOptionAndTitleContaining(boolean publicOption, String title, Sort sort);
 
 }
