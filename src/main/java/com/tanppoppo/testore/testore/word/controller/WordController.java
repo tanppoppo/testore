769edea623b434085077817b6cb61825e5481ffc
gyahury <rudgns1243@naver.com>
2024-10-18 15:35:18 +0900

    Refacotr: 내가 좋아한 시험지, 단어장 컨트롤러 위치 변경
    - 내가 좋아한 시험지, 단어장 컨트롤러 위치 변경
    - 공유 수를 학습, 응시로 변경
    - 응시한 목록 페이지 추가

--- src/main/java/com/tanppoppo/testore/testore/word/controller/WordController.java
+++ src/main/java/com/tanppoppo/testore/testore/word/controller/WordController.java
@@ -445,39 +445,5 @@ public class WordController {
 
     }
 
-    /**
-     * 내가 좋아요한 단어장 조회
-     * @author KIMGEON64
-     * @param user 인증된 회원 정보를 전달합니다.
-     * @param model 모델 객체를 전달합니다.
-     * @return 내가 좋아요한 단어장 조회 페이지를 반환합니다.
-     */
-    @GetMapping("likedWordBook")
-    public String likedWordBook(@AuthenticationPrincipal AuthenticatedUser user, Model model){
-
-        List<WordBookDTO> items = ws.getLikedWordBook(user);
-        model.addAttribute("items", items);
-
-        return "word/word-likedWordBook";
-
-    }
-
-    /**
-     * 내가 북마크한 단어장 조회
-     * @author KIMGEON64
-     * @param user 인증된 회원 정보를 전달합니다.
-     * @param model 모델 객체를 전달합니다.
-     * @return 내가 북마크한 단어장 조회 페이지를 반환합니다.
-     */
-    @GetMapping("bookmarkedWordBook")
-    public String bookmarkedWordBook(@AuthenticationPrincipal AuthenticatedUser user, Model model){
-
-        List<WordBookDTO> items = ws.getBookmarkedWordBook(user);
-        model.addAttribute("items", items);
-
-        return "word/word-bookmarkedWordBook";
-
-    }
-
 
 }
